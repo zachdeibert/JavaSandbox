9350a0542f08d22b83afcf668d905d64a185a2bc
Zach Deibert <zachdeibert@gmail.com>
2016-05-28 00:58:03 +0000

    Moved some code

--- src/main/java/com/github/zachdeibert/javasandbox/SandboxTransformer.java
+++ src/main/java/com/github/zachdeibert/javasandbox/SandboxTransformer.java
@@ -10,7 +10,8 @@ final class SandboxTransformer extends ClassVisitor {
 	private final JvmSubset subset;
 	private final String prefix;
 
-	private final String transform(final String cls) {
+	private final String transformClass(String cls) {
+		cls = cls.replace('/', '.');
 		if ( subset.isIncluded(cls) ) {
 			if ( subset.isAllowed(cls) ) {
 				return cls;
@@ -22,13 +23,39 @@ final class SandboxTransformer extends ClassVisitor {
 		}
 	}
 
+	private final String transformClassDesc(String desc) {
+		return transformClass(desc).replace('.', '/');
+	}
+
+	private final Type transform(final Type cls) {
+		switch ( cls.getSort() ) {
+		case Type.ARRAY:
+			return Type.getType("[".concat(transform(cls.getElementType()).getDescriptor()));
+		case Type.METHOD:
+			Type ret = transform(cls.getReturnType());
+			Type[] argTypes = cls.getArgumentTypes();
+			for ( int i = 0; i < argTypes.length; ++i ) {
+				argTypes[i] = transform(argTypes[i]);
+			}
+			return Type.getMethodType(ret, argTypes);
+		case Type.OBJECT:
+			return Type.getObjectType(transformClass(cls.getClassName()).replace('.', '/'));
+		default:
+			return cls;
+		}
+	}
+
+	private final String transformMethodDesc(final String desc) {
+		return transform(Type.getMethodType(desc)).getDescriptor();
+	}
+
 	@Override
 	public final void visit(final int version, final int access, String name, final String signature, String superName,
 			final String[] interfaces) {
-		name = transform(name);
-		superName = transform(superName);
+		name = transformClassDesc(name);
+		superName = transformClassDesc(superName);
 		for ( int i = 0; i < interfaces.length; ++i ) {
-			interfaces[i] = transform(interfaces[i]);
+			interfaces[i] = transformClass(interfaces[i]);
 		}
 		super.visit(version, access, name, signature, superName, interfaces);
 	}
@@ -36,22 +63,17 @@ final class SandboxTransformer extends ClassVisitor {
 	@Override
 	public final FieldVisitor visitField(final int access, final String name, String desc, final String signature,
 			final Object value) {
-		desc = transform(Type.getType(desc).getClassName());
+		desc = transformClassDesc(desc);
 		return super.visitField(access, name, desc, signature, value);
 	}
 
 	@Override
 	public final MethodVisitor visitMethod(final int access, final String name, String desc, final String signature,
 			final String[] exceptions) {
-		String ret = transform(Type.getReturnType(desc).getClassName());
-		Type[] argTypes = Type.getArgumentTypes(desc);
-		for ( int i = 0; i < argTypes.length; ++i ) {
-			argTypes[i] = Type.getObjectType(transform(argTypes[i].getClassName()).replace('.', '/'));
-		}
-		desc = Type.getMethodDescriptor(Type.getObjectType(ret.replace('.', '/')), argTypes);
+		desc = transformMethodDesc(desc);
 		if ( exceptions != null ) {
 			for ( int i = 0; i < exceptions.length; ++i ) {
-				exceptions[i] = transform(exceptions[i]);
+				exceptions[i] = transformClass(exceptions[i]);
 			}
 		}
 		return super.visitMethod(access, name, desc, signature, exceptions);
