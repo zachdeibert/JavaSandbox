0e607b251852d3c155fb0e381b2e7090d30b4a51
Zach Deibert <zachdeibert@gmail.com>
2016-05-30 21:08:20 +0000

    Finished writing initial version of SandboxTransformer

--- src/main/java/com/github/zachdeibert/javasandbox/SandboxTransformer.java
+++ src/main/java/com/github/zachdeibert/javasandbox/SandboxTransformer.java
@@ -1,12 +1,89 @@
 package com.github.zachdeibert.javasandbox;
 
+import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Handle;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.TypePath;
 
 final class SandboxTransformer extends ClassVisitor {
+	private final class Method extends MethodVisitor {
+		@Override
+		public final AnnotationVisitor visitTypeAnnotation(final int typeRef, final TypePath typePath, String desc,
+				final boolean visible) {
+			desc = transformClassDesc(desc);
+			return super.visitTypeAnnotation(typeRef, typePath, desc, visible);
+		}
+
+		@Override
+		public final AnnotationVisitor visitParameterAnnotation(final int parameter, String desc,
+				final boolean visible) {
+			desc = transformClassDesc(desc);
+			return super.visitParameterAnnotation(parameter, desc, visible);
+		}
+
+		@Override
+		public final void visitTypeInsn(final int opcode, String type) {
+			type = transformClassDesc(type);
+			super.visitTypeInsn(opcode, type);
+		}
+
+		@Override
+		public final void visitFieldInsn(final int opcode, String owner, final String name, String desc) {
+			owner = transformClassDesc(owner);
+			desc = transformClassDesc(desc);
+			super.visitFieldInsn(opcode, owner, name, desc);
+		}
+
+		@Override
+		public final void visitMethodInsn(final int opcode, String owner, final String name, String desc,
+				final boolean itf) {
+			owner = transformClassDesc(owner);
+			desc = transformMethodDesc(desc);
+			super.visitMethodInsn(opcode, owner, name, desc, itf);
+		}
+
+		@Override
+		public final void visitInvokeDynamicInsn(final String name, String desc, Handle bsm, final Object... bsmArgs) {
+			desc = transformMethodDesc(desc);
+			bsm = transformHandle(bsm);
+			for ( int i = 0; i < bsmArgs.length; ++i ) {
+				if ( bsmArgs[i] instanceof Handle ) {
+					bsmArgs[i] = transformHandle((Handle) bsmArgs[i]);
+				}
+			}
+			super.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs);
+		}
+
+		@Override
+		public final void visitMultiANewArrayInsn(String desc, final int dims) {
+			desc = transformClassDesc(desc);
+			super.visitMultiANewArrayInsn(desc, dims);
+		}
+
+		@Override
+		public final AnnotationVisitor visitInsnAnnotation(final int typeRef, final TypePath typePath, String desc,
+				final boolean visible) {
+			desc = transformClassDesc(desc);
+			return super.visitInsnAnnotation(typeRef, typePath, desc, visible);
+		}
+
+		@Override
+		public final void visitLocalVariable(final String name, String desc, final String signature, final Label start,
+				final Label end, final int index) {
+			desc = transformClassDesc(desc);
+			super.visitLocalVariable(name, desc, signature, start, end, index);
+		}
+
+		public Method(final MethodVisitor mv) {
+			super(Opcodes.ASM5, mv);
+		}
+	}
+
 	private final JvmSubset subset;
 	private final String prefix;
 
@@ -24,7 +101,17 @@ final class SandboxTransformer extends ClassVisitor {
 	}
 
 	private final String transformClassDesc(String desc) {
-		return transformClass(desc).replace('.', '/');
+		boolean t;
+		if ( desc.length() == 1 ) {
+			return desc;
+		} else if ( (t = desc.startsWith("[") || desc.endsWith(";")) ) {
+			desc = Type.getType(desc).getClassName();
+		}
+		desc = transformClass(desc).replace('.', '/');
+		if ( t ) {
+			desc = Type.getObjectType(desc).getDescriptor();
+		}
+		return desc;
 	}
 
 	private final Type transform(final Type cls) {
@@ -47,6 +134,11 @@ final class SandboxTransformer extends ClassVisitor {
 
 	private final String transformMethodDesc(final String desc) {
 		return transform(Type.getMethodType(desc)).getDescriptor();
+	}
+
+	private final Handle transformHandle(final Handle h) {
+		return new Handle(h.getTag(), transformClassDesc(h.getOwner()), h.getName(), transformMethodDesc(h.getDesc()),
+				h.isInterface());
 	}
 
 	@Override
@@ -73,10 +165,10 @@ final class SandboxTransformer extends ClassVisitor {
 		desc = transformMethodDesc(desc);
 		if ( exceptions != null ) {
 			for ( int i = 0; i < exceptions.length; ++i ) {
-				exceptions[i] = transformClass(exceptions[i]);
+				exceptions[i] = transformClassDesc(exceptions[i]);
 			}
 		}
-		return super.visitMethod(access, name, desc, signature, exceptions);
+		return new Method(super.visitMethod(access, name, desc, signature, exceptions));
 	}
 
 	SandboxTransformer(final ClassVisitor cv, final Sandbox sandbox) {
